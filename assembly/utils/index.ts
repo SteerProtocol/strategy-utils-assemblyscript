fecf10f4fc15aed8b1df8f0808f8ba8c8ca08efc
Derek Barrera <derekbarrera@gmail.com>
2023-01-31 09:52:13 -0500

    add missing export

--- assembly/utils/index.ts
+++ assembly/utils/index.ts
@@ -2,5 +2,6 @@ export * from './Math';
 export * from './MovingAverages';
 export * from './Ranges';
 export * from './UniswapLiquidityUtils';
+export * from './SlidingWindow';
 export * from './console';
 export * from './types';

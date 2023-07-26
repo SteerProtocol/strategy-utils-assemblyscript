510583295e2f9b94d0091ff9944daf6c84eb9196
Brandon <brandonjmhunt@gmail.com>
2023-07-26 15:34:28 -0700

    feat: :zap: Trigger logic and tests
    last test due to failing wasm -- joinIntegerArray i64 err

--- assembly/utils/triggers.ts
+++ assembly/utils/triggers.ts
@@ -1,6 +1,11 @@
-import { Position } from "./types";
+import { Position } from "@steerprotocol/strategy-utils/assembly";
+import { JSON } from "json-as";
 
 // NOTE: Trigger functions return true when action should be taken, if false then the strategy can return 'continue' to skip exeuction
+// Implementation might look like the following:
+  // const trigger = getTriggerStyle(configJson.triggerStyle)
+  // const triggerObj = new TriggerConfigHelper(configJson.triggerWhenOver, configJson.tickPriceTrigger, configJson.percentageOfPositionRangeToTrigger, configJson.tickDistanceFromCenter, configJson.elapsedTendTime)
+  // if (!shouldTriggerExecution(trigger, triggerObj, _positions, _currentTick, _timeSinceLastExecution)) return 'continue'
 
 // Gets active range from the output of LM.getPositions()
 export function parseActiveRange(_positions: string): Position {
@@ -61,22 +66,298 @@ export function triggerPositionsInactive(currentPosition: Position, currentTick:
 }
 
 // when tick goes over or under specified tick, trigger rebalance
-export function triggerFromSpecifiedPrice(triggerTick: i64, currentTick: i64, triggerOver: boolean): boolean {
-    // if trigger over, return true when currentTick > triggerTick
-    if (triggerOver) {
-        return currentTick > triggerTick
-    }
-    // trigger under, if current price is less than trigger tick return true
-    return triggerTick > currentTick
-}
+// export function triggerFromSpecifiedPrice(triggerTick: i64, currentTick: i64, triggerOver: boolean): boolean {
+//     // if trigger over, return true when currentTick > triggerTick
+//     if (triggerOver) {
+//         return currentTick > triggerTick
+//     }
+//     // trigger under, if current price is less than trigger tick return true
+//     return triggerTick > currentTick
+// }
 
 // when tick goes over or under current positions, trigger rebalance, only moves one way
 export function triggerPricePastPositions(currentPosition: Position, currentTick: i64, triggerOver: boolean): boolean {
     if (emptyCurrentPosition(currentPosition)) return true
     if (triggerOver) {
         // if the currentTick is over the endTick, rebal
-        return currentTick > currentPosition.endTick
+        return currentTick > i64(currentPosition.endTick)
     }
     // if current tick is under start tick
-    return currentTick < currentPosition.startTick
+    return currentTick < i64(currentPosition.startTick)
+}
+
+export const enum TriggerStyle {
+    DistanceFromCenterOfPositions,
+    PercentageChangeFromPositionRange,
+    PositionsInactive,
+    // SpecificPrice,
+    PricePastPositions,
+    None,
+}
+
+// export function TriggerStyleLookup(triggerStyle: TriggerStyle): string {
+//     switch (triggerStyle) {
+//         case TriggerStyle.DistanceFromCenterOfPositions:
+//             return "Distance from center of position(s)";
+//         case TriggerStyle.PercentageChangeFromPositionRange:
+//             return "Percentage of position(s)";
+//         case TriggerStyle.PositionsInactive:
+//             return "Positions Inactive";
+//         // case TriggerStyle.SpecificPrice:
+//         //     return "Specific Price";
+//         case TriggerStyle.PricePastPositions:
+//             return "Price moved past position(s)";
+//         case TriggerStyle.None:
+//           return 'None'
+
+//     default:
+//         throw new Error(`Unknown trigger style: ${triggerStyle}`);
+//     }
+// }
+
+// export class DistanceFromCenterOfPositionsOptions {
+//     tickDistanceFromCenter: i64 = 0;
+//     requiredDataTypes: string[] = ['Liquidity Manager Positions', 'V3 Pool Current Tick'];
+// }
+
+// export class PercentageChangeFromPositionRangeOptions {
+//     percentageOfPositionRangeToTrigger: f64 = 0.0;
+//     requiredDataTypes: string[] = ['Liquidity Manager Positions', 'V3 Pool Current Tick'];
+// }
+
+// export class PositionsInactiveOptions {
+//     requiredDataTypes: string[] = ['Liquidity Manager Positions', 'V3 Pool Current Tick'];
+// }
+
+// export class SpecificPriceOptions {
+//     tickPriceTrigger: i64 = 0;
+//     triggerWhenOver: boolean = false;
+//     requiredDataTypes: string[] = ['V3 Pool Current Tick'];
+// }
+
+// export class PricePastPositionsOptions {
+//     triggerWhenOver: boolean = false;
+//     requiredDataTypes: string[] = ['Liquidity Manager Positions', 'V3 Pool Current Tick'];
+// }
+
+export class TriggerConfigHelper {
+    // triggerType: string = "Price leaves active range";
+    triggerWhenOver: boolean = false;
+    tickPriceTrigger: i64 = 0;
+    percentageOfPositionRangeToTrigger: f64 = 0.0;
+    tickDistanceFromCenter: i64 = 0;
+    elapsedTendTime: i64 = 0;
+    constructor( t: boolean, tpt: i64, poptrr:f64, tdfc: i64, ett: i64) {
+      this.triggerWhenOver = t
+      this.tickPriceTrigger = tpt
+      this.percentageOfPositionRangeToTrigger = poptrr
+      this.tickDistanceFromCenter = tdfc
+      this.elapsedTendTime = ett
+    }
+}
+
+// no AS func overloading yet :(
+// export function shouldTriggerExecution_1(triggerStyle: TriggerStyle, triggerOptions: TriggerConfigHelper, dataConnector1: string) : boolean {
+//     // parse options
+//     switch (triggerStyle) {
+//         case TriggerStyle.SpecificPrice:
+//             // parse current tick from dc1
+//             const currentTick = parseInt(dataConnector1)
+//             if (!currentTick) return true
+//             // tick and over
+//             return triggerFromSpecifiedPrice(triggerOptions.tickPriceTrigger, currentTick, triggerOptions.triggerWhenOver)
+//         default:
+//             return true
+//     }
+// }
+
+export function shouldTriggerExecution(triggerStyle: TriggerStyle, triggerOptions: TriggerConfigHelper, dataConnector1: string, dataConnector2: string, dataConnector3: string) : boolean {
+
+    // possible dc inputs
+    let currentPositionRange: Position;
+    let currentTick: i64;
+
+    // auto trigger if we are overdue
+    const timeSinceLastExecution = i64(parseInt(dataConnector3))
+    if (timeSinceLastExecution >= i64(triggerOptions.elapsedTendTime)) return true
+
+    switch (triggerStyle) {
+        case TriggerStyle.DistanceFromCenterOfPositions:
+            // parse ulm positions [0], current tick [1]
+            currentPositionRange = parseActiveRange(dataConnector1)
+            currentTick = i64(parseInt(dataConnector2))
+            if (!currentTick) return true
+            return triggerFromDistance(currentPositionRange, triggerOptions.tickDistanceFromCenter, currentTick)
+
+        case TriggerStyle.PercentageChangeFromPositionRange:
+            // parse ulm positions [0], current tick [1]
+            currentPositionRange= parseActiveRange(dataConnector1)
+            currentTick = i64(parseInt(dataConnector2))
+            if (!currentTick) return true
+            return triggerFromPercentage(currentPositionRange, triggerOptions.percentageOfPositionRangeToTrigger, currentTick)
+
+        case TriggerStyle.PositionsInactive:
+            // parse ulm positions [0], current tick [1]
+            currentPositionRange = parseActiveRange(dataConnector1)
+            currentTick = i64(parseInt(dataConnector2))
+            if (!currentTick) return true
+            return triggerPositionsInactive(currentPositionRange, currentTick)
+
+        case TriggerStyle.PricePastPositions:
+            // parse ulm positions [0], current tick [1]
+            currentPositionRange = parseActiveRange(dataConnector1)
+            currentTick = i64(parseInt(dataConnector2))
+            if (!currentTick) return true
+            return triggerPricePastPositions(currentPositionRange, currentTick,  triggerOptions.triggerWhenOver)
+        default:
+            return true
+    }
+}
+
+export function getTriggerStyle(trigger: string): TriggerStyle {
+  if (trigger === 'Current Price set distance from center of positions') {
+      return TriggerStyle.DistanceFromCenterOfPositions;
+  } else if (trigger === 'Price leaves active range') {
+      return TriggerStyle.PositionsInactive;
+  } else if (trigger === 'Price moves percentage of active range away') {
+      return TriggerStyle.PercentageChangeFromPositionRange;
+  } else if (trigger === 'Price moves one way past positions') {
+      return TriggerStyle.PricePastPositions;
+  } else {
+      return TriggerStyle.None;
+  }
+}
+
+function getTriggerName(trigger: TriggerStyle): string {
+  if (trigger === TriggerStyle.DistanceFromCenterOfPositions) {
+      return 'Current Price set distance from center of positions';
+  } else if (trigger === TriggerStyle.PositionsInactive) {
+      return 'Price leaves active range';
+  } else if (trigger === TriggerStyle.PercentageChangeFromPositionRange) {
+      return 'Price moves percentage of active range away';
+  } else if (trigger === TriggerStyle.PricePastPositions) {
+      return 'Price moves one way past positions';
+  } else {
+      return 'None';
+  }
+}
+
+
+export function triggerPropertyHelper(omit: TriggerStyle[] = []): string {
+    const triggerList = [
+        TriggerStyle.DistanceFromCenterOfPositions,
+        TriggerStyle.None,
+        TriggerStyle.PercentageChangeFromPositionRange,
+        TriggerStyle.PositionsInactive,
+        TriggerStyle.PricePastPositions,
+    ];
+
+    const filteredTriggers: string[] = [
+      'Current Price set distance from center of positions',
+      'Price leaves active range',
+      'Price moves percentage of active range away',
+      'Price moves one way past positions',
+      'None'
+    ];
+    // for(let trigger = 0; trigger < triggerList.length; trigger++){
+    //   if (!omit.includes(  triggerList[trigger])) {
+    //     filteredTriggers.push(getTriggerName(triggerList[trigger]));
+    //   }
+    // }
+
+    return `"triggerStyle": {
+      "enum": ${JSON.stringify(filteredTriggers)},
+      "title": "Logic to trigger new positions",
+      "type": "string",
+      "default": "None"
+    }`;
+  }
+
+  export function allOfTrigger(): string {
+    return `{
+    "if": {
+      "properties": {
+        "triggerStyle": {
+          "const": "None"
+        }
+      }
+    },
+    "then": {
+      "properties": {},
+      "required": []
+    }
+  },
+  {
+    "if": {
+      "properties": {
+        "triggerStyle": {
+          "const": "Current Price set distance from center of positions"
+        }
+      }
+    },
+    "then": {
+      "properties": {
+        "tickDistanceFromCenter": {
+            "type": "integer",
+            "title": "Tick Distance",
+            "description": "The number of ticks (basis points) from center price of positions to trigger setting new positions",
+            "detailedDescription": "The static number of ticks from the center of the active range to trigger: if our position goes from 0-100, and we have a tick distance of 75, we will go out 75 ticks both ways from the center of our positions (50). This means we will skip execution only if the current tick is between -25 and 125. Future positions will determine where the center of the trigger range is located."
+        }
+      },
+      "required": ["tickDistanceFromCenter"]
+    }
+  },
+  {
+    "if": {
+      "properties": {
+        "triggerStyle": {
+          "const": "Price leaves active range"
+        }
+      }
+    },
+    "then": {
+      "properties": {},
+      "required": []
+    }
+  },
+  {
+    "if": {
+      "properties": {
+        "triggerStyle": {
+          "const": "Price moves percentage of active range away"
+        }
+      }
+    },
+    "then": {
+      "properties": {
+        "percentageOfPositionRangeToTrigger": {
+            "type": "number",
+            "title": "Percentage of Range",
+            "description": "The percentage of the range away to trigger new positions, 100% or 1 would be at the bounds of the range",
+            "detailedDescription": "If you have a simple position ranging from ticks 0 - 100, and you set this value to 1, the trigger range will be the outer bounds. Using 0.5 would make the trigger range 25-75, 2 would make the range -50 - 150."
+        }
+      },
+      "required": ["percentageOfPositionRangeToTrigger"]
+    }
+  },
+  {
+    "if": {
+      "properties": {
+        "triggerStyle": {
+          "const": "Price moves one way past positions"
+        }
+      }
+    },
+    "then": {
+      "properties": {
+        "triggerWhenOver": {
+            "type": "boolean",
+            "title": "Price Moves Higher",
+            "description": "True for if the strategy should set new positions when the price (tick) is higher than the current positions, false for lower",
+            "detailedDescription": "If our current position ranges from ticks 0 - 100, true will make our bundle execute only when the current tick is higher. Any other case (current tick less than 100) will result in a continue recommendation."
+        }
+      },
+      "required": ["triggerWhenOver"]
+    }
+  }`;
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,226 @@
-const myModule = require("../index");
+import {  candles } from "./utils";
+import fs from 'fs';
+import { WasmModule, loadWasm } from "@steerprotocol/app-loader";
 
-describe("Util Tests", () => {
-    it("", async () => {
-      expect(true)
+
+describe("Unit tests", () => {
+  let myModule: WasmModule;
+  
+  beforeEach(async () => {
+    myModule = await loadWasm(fs.readFileSync(__dirname + "/debug.wasm"), {})
+  });
+
+    describe("Trigger tests", () => {
+      // Testing the following options
+      // 'Current Price set distance from center of positions',
+      // 'Price leaves active range',
+      // 'Price moves percentage of active range away',
+      // 'Price moves one way past positions',
+      // 'None'
+      test("Can return positions on none type", async () => {        
+        const config = `{
+          "elapsedTendTime": 604800,
+          "triggerStyle": "None",
+          "strategy": "Classic",
+          "liquidityShape": "Linear",
+          "poolFee": 3000,
+          "placementType": "Position over current price",
+          "positionSize": 600
+        }`
+        myModule.initialize(config);
+        const positions = '[[257100],[257300],[1]]'
+        const currentTick = '257301'
+        const timeSinceLastExecution = '5600'
+        const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+        (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+        expect(result).not.toEqual('continue')
+      });
+
+      test("Can return continue for active trigger", async () => {        
+          const config = `{
+            "elapsedTendTime": 604800,
+            "triggerStyle": "Price leaves active range",
+            "strategy": "Classic",
+            "liquidityShape": "Linear",
+            "poolFee": 500,
+            "placementType": "Position over current price",
+
+            "positionSize": 600
+          }`
+          myModule.initialize(config);
+          const positions = '[[257100],[257300],[1]]'
+          const currentTick = '257251'
+          const timeSinceLastExecution = '5600'
+          const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+          (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+          expect(result).toEqual('continue')
+        });
+
+        test("Can return positions for active trigger", async () => {        
+          const config = `{
+            "elapsedTendTime": 604800,
+            "triggerStyle": "Price leaves active range",
+            "strategy": "Classic",
+            "liquidityShape": "Linear",
+            "poolFee": 3000,
+            "placementType": "Position over current price",
+
+            "positionSize": 600
+          }`
+          myModule.initialize(config);
+          const positions = '[[257100],[257300],[1]]'
+          const currentTick = '257301'
+          const timeSinceLastExecution = '5600'
+          const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+          (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+          expect(result).not.toEqual('continue')
+        });
+
+        test("Can return continue for distance trigger", async () => {        
+          const config = `{
+            "elapsedTendTime": 604800,
+            "triggerStyle": "Current Price set distance from center of positions",
+            "tickDistanceFromCenter": 100,
+            "strategy": "Classic",
+            "liquidityShape": "Linear",
+            "poolFee": 3000,
+            "placementType": "Position over current price",
+
+            "positionSize": 600
+          }`
+          myModule.initialize(config);
+          const positions = '[[257100],[257300],[1]]'
+          const currentTick = '257251'
+          const timeSinceLastExecution = '5600'
+          const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+          (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+          expect(result).toEqual('continue')
+        });
+
+        test("Can return positions for distance trigger", async () => {        
+          const config = `{
+            "elapsedTendTime": 604800,
+            "triggerStyle": "Current Price set distance from center of positions",
+            "tickDistanceFromCenter": 100,
+            "strategy": "Classic",
+            "liquidityShape": "Linear",
+            "poolFee": 3000,
+            "placementType": "Position over current price",
+
+            "positionSize": 600
+          }`
+          myModule.initialize(config);
+          const positions = '[[257100],[257300],[1]]'
+          const currentTick = '257301'
+          const timeSinceLastExecution = '5600'
+          const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+          (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+          expect(result).not.toEqual('continue')
+        });
+
+        test("Can return continue for percentage trigger", async () => {        
+          const config = `{
+            "elapsedTendTime": 604800,
+            "triggerStyle": "Price moves percentage of active range away",
+            "percentageOfPositionRangeToTrigger": 1,
+            "strategy": "Classic",
+            "liquidityShape": "Linear",
+            "poolFee": 3000,
+            "placementType": "Position over current price",
+
+            "positionSize": 600
+          }`
+          myModule.initialize(config);
+          const positions = '[[257100],[257300],[1]]'
+          const currentTick = '257251'
+          const timeSinceLastExecution = '5600'
+          const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+          (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+          expect(result).toEqual('continue')
+        });
+
+        test("Can return positions for percentage trigger", async () => {        
+          const config = `{
+            "elapsedTendTime": 604800,
+            "triggerStyle": "Price moves percentage of active range away",
+            "percentageOfPositionRangeToTrigger": 1,
+            "strategy": "Classic",
+            "liquidityShape": "Linear",
+            "poolFee": 3000,
+            "placementType": "Position over current price",
+
+            "positionSize": 600
+          }`
+          myModule.initialize(config);
+          const positions = '[[257100],[257300],[1]]'
+          const currentTick = '257301'
+          const timeSinceLastExecution = '5600'
+          const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+          (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+          expect(result).not.toEqual('continue')
+        });
+
+        test("Can return continue for one way inactive - active", async () => {        
+          const config = `{
+            "elapsedTendTime": 604800,
+            "triggerStyle": "Price moves one way past positions",
+            "triggerWhenOver": true,
+            "strategy": "Classic",
+            "liquidityShape": "Linear",
+            "poolFee": 3000,
+            "placementType": "Position over current price",
+            "triggerWhenOver": true,
+            "positionSize": 600
+          }`
+          myModule.initialize(config);
+          const positions = '[[257100],[257300],[1]]'
+          const currentTick = '257251'
+          const timeSinceLastExecution = '5600'
+          const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+          (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+          expect(result).toEqual('continue')
+        });
+
+        test("Can return continue for one way inactive - below", async () => {        
+          const config = `{
+            "elapsedTendTime": 604800,
+            "triggerStyle": "Price moves one way past positions",
+            "triggerWhenOver": true,
+            "strategy": "Classic",
+            "liquidityShape": "Linear",
+            "poolFee": 3000,
+            "placementType": "Position over current price",
+            "triggerWhenOver": true,
+            "positionSize": 600
+          }`
+          myModule.initialize(config);
+          const positions = '[[257100],[257300],[1]]'
+          const currentTick = '257000'
+          const timeSinceLastExecution = '5600'
+          const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+          (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+          expect(result).toEqual('continue')
+        });
+
+        test("Can return positions for one way inactive - above", async () => {        
+          const config = `{
+            "elapsedTendTime": 604800,
+            "triggerStyle": "Price moves one way past positions",
+            "triggerWhenOver": true,
+            "strategy": "Classic",
+            "liquidityShape": "Linear",
+            "poolFee": 500,
+            "placementType": "Position over current price",
+            "triggerWhenOver": true,
+            "positionSize": 100
+          }`
+          myModule.initialize(config);
+          const positions = '[[257100],[257300],[1]]'
+          const currentTick = '257301'
+          const timeSinceLastExecution = '5600'
+          const result = myModule["execute(param_1: string, param_2: string, param_3: string, param_4: string)"]
+          (JSON.stringify(candles), positions, currentTick, timeSinceLastExecution);
+          expect(result).not.toEqual('continue')
+        });
     });
 });
